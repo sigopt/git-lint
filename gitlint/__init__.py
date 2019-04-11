13c4639f31d9f1b51ad9c6c402c81dfdba8d7312
Dan W Anderson <dan@sigopt.com>
2019-04-11 15:45:16 -0700

    fix-yaml-warning (#3)

--- gitlint/__init__.py
+++ gitlint/__init__.py
@@ -102,7 +102,7 @@ def get_config(repo_root):
         if not content:
             yaml_config = {}
         else:
-            yaml_config = yaml.load(content)
+            yaml_config = yaml.load(content, Loader=yaml.FullLoader)
 
     return linters.parse_yaml_config(yaml_config, repo_root)
 
